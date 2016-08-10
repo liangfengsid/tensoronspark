9031abc84c107e20b88abcc46901ea6a748d1ba9
liangfengsid <liangfengsid@163.com>
2016-08-10 16:33:37 +0800

    add DeltaWeightCombiner

--- tensorspark/core/weight_combiner.py
+++ tensorspark/core/weight_combiner.py
@@ -3,7 +3,7 @@ class WeightCombiner(object):
 		pass
 
 
-	def compute(self, origin_weight, new_weight):
+	def compute(self, origin_weight, new_weight, worker_id=-1):
 		raise NotImplementedError('method not implemented')
 
 
@@ -14,11 +14,10 @@ class WeightCombiner(object):
 class MeanWeightCombiner(WeightCombiner):
 	def __init__(self, num):	
 		super(WeightCombiner, self).__init__()	
-		print('test')
 		self._num = num
 
 
-	def compute(self, origin_weight, new_weight):
+	def compute(self, origin_weight, new_weight, worker_id=-1):
 		updated_weight = ((self._num - 1) * origin_weight + new_weight) / self._num
 		return updated_weight
 
@@ -28,8 +27,33 @@ class UpdateMeanWeightCombiner(WeightCombiner):
 		self._server = server
 
 
-	def compute(self, origin_weight, new_weight):
+	def compute(self, origin_weight, new_weight, worker_id=-1):
 		if self._server._version == 0:
 			return new_weight
 		else:
 			return (origin_weight + new_weight) / 2
+
+
+class DeltaWeightCombiner(WeightCombiner,):
+	def __init__(self):
+		super(WeightCombiner, self).__init__()
+		self.last_weight = {}
+
+
+	def compute(self, origin_weight, new_weight, worker_id=-1):
+		# Param new_weight stands for the delta from the original weight
+		if worker_id == -1:
+			raise ValueError('Not recognized worker in DeltaWeightCombiner')
+		try:
+			weight = self.last_weight[worker_id]
+			delta_weight = new_weight - weight
+		except KeyError:
+			delta_weight = new_weight
+			
+		updated_weight = origin_weight + delta_weight
+		self.last_weight[worker_id] = new_weight
+		return updated_weight
+
+
+	def reset_value():
+		self.last_weight = {}
---
+++ tensorspark/test/test_combiner.py
@@ -0,0 +1,21 @@
+import tensorspark.core.weight_combiner as comb
+import numpy as np
+
+def test_delta_weight_combiner():
+	combiner = comb.DeltaWeightCombiner()
+	origin = np.array([0, 0, 0])
+	updated_1 = np.array([2, 2, 2])
+	updated_2 = np.array([3, 3, 2])
+	updated_3 = np.array([2, 4, 3])
+	result1 = combiner.compute(origin, updated_1, worker_id='1')
+	result2 = combiner.compute(result1, updated_2, worker_id='2')
+	result3 = combiner.compute(result2, updated_3, worker_id='2')
+	valid_result1 = np.array([2, 2, 2])
+	valid_result2 = np.array([5, 5, 4])
+	valid_result3 = np.array([4, 6, 5])
+	assert(list(result1) == list(valid_result1))
+	assert(list(result2) == list(valid_result2))
+	assert(list(result3) == list(valid_result3))
+
+
+#test_delta_weight_combiner()
