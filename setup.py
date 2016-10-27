70eacccd94b5747605681a827788430b73398666
liangfengsid <liangfengsid@163.com>
2016-10-28 03:05:00 +0800

    Version 1.0.6

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ except ImportError:
 
 setup(
 	name = "tensorspark",
-	version = "1.0.4",
+	version = "1.0.6",
 	author = "Liang Feng",
 	author_email = "loengf@connect.hku.hk",
 	description = ("Tensorflow on Spark, a scalable system for high-performance machine learning"),
 	license = "Apache",
 	keywords = "",
 	url = "https://github.com/liangfengsid/tensoronspark/",
-	packages=['tensorspark', 'tensorspark.core', 'tensorspark.example'],
+	packages=['tensorspark', 'tensorspark.core', 'tensorspark.example', 'tensorspark.test'],
 	package_data={'tensorspark':['MNIST_data/*-ubyte']}, 
 	classifiers=[
 		'Development Status :: 4 - Beta',
